be61765f56ad44610a8c5b04b4ba7b59622e8d9c
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-04-01 19:59:29 +0700

    Edit tools of sketchbook

--- app/src/main/java/com/rujirakongsomran/jc_sketchbooklib/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_sketchbooklib/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.rujirakongsomran.jc_sketchbooklib
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_sketchbooklib.ui.theme.JC_SketchbookLibTheme
@@ -37,8 +41,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateSketchbook() {
+    val context = LocalContext.current
     val sketchbookController = rememberSketchbookController()
-
 
     Column(
         modifier = Modifier
@@ -74,62 +78,73 @@ fun CreateSketchbook() {
             },
         )
         Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            IconButton(
-                onClick = {
-                    sketchbookController.undo()
-                }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_undo),
-                    contentDescription = null,
-                    tint = Color.White
-                )
-            }
-            IconButton(
-                onClick = {
-                    sketchbookController.redo()
-                }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_redo),
-                    contentDescription = null,
-                    tint = Color.White
-                )
-            }
-            IconButton(
-                onClick = {
-                    sketchbookController.setRainbowShader()
-                }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_brush),
-                    contentDescription = null,
-                    tint = Color.White
-                )
-            }
-            IconButton(
-                onClick = {
-                    sketchbookController.toggleEraseMode()
-                    val isEraseMode = sketchbookController.isEraseMode.value
-                    sketchbookController.setEraseMode(isEraseMode)
-                    sketchbookController.setEraseRadius(50f)
-                }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_eraser),
-                    contentDescription = null,
-                    tint = Color.White
-                )
-            }
-            IconButton(
-                onClick = {
-                    sketchbookController.clearPaths()
-                }) {
-                Icon(
-                    Icons.Rounded.Delete,
-                    contentDescription = null,
-                    tint = Color.White
-                )
-            }
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_undo),
+                contentDescription = null,
+                modifier = Modifier
+                    .clickable { sketchbookController.undo() }
+                    .size(30.dp),
+                tint = if (sketchbookController.canUndo.value) {
+                    MaterialTheme.colors.primary
+                } else {
+                    MaterialTheme.colors.onPrimary
+                }
+            )
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_redo),
+                contentDescription = null,
+                modifier = Modifier
+                    .clickable { sketchbookController.redo() }
+                    .size(30.dp),
+                tint = if (sketchbookController.canRedo.value) {
+                    MaterialTheme.colors.primary
+                } else {
+                    MaterialTheme.colors.onPrimary
+                }
+            )
+
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_brush),
+                contentDescription = null,
+                modifier = Modifier
+                    .clickable {
+                        sketchbookController.setEraseMode(false)
+                        sketchbookController.setRainbowShader()
+                        Toast
+                            .makeText(context, "Rainbow Shader", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    .size(30.dp),
+                tint = MaterialTheme.colors.primary
+            )
+            Image(
+                bitmap = ImageBitmap.imageResource(id = R.drawable.ic_eraser),
+                contentDescription = null,
+                modifier = Modifier
+                    .clickable {
+                        sketchbookController.toggleEraseMode()
+                        Toast
+                            .makeText(context, "Eraser Mode", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    .size(30.dp),
+            )
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_clear),
+                contentDescription = null,
+                modifier = Modifier
+                    .clickable {
+                        sketchbookController.clear()
+                    }
+                    .size(30.dp),
+                tint = Color.LightGray
+            )
         }
     }
 }
